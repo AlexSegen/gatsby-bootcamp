21d2c1a715b977552bcd824cc7b089791f7514da
Alejandro Vivas <ajvivas86@gmail.com>
2020-03-28 15:20:27 -0300

    11. Working with Markdown Posts: Challenge

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import Layout from '../components/layout';
 
+import { graphql, useStaticQuery } from 'gatsby'
+
 const Blog =() => {
+
+    const data = useStaticQuery(graphql`
+    query {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                title
+                date
+              }
+              html
+              excerpt
+            }
+          }
+        }
+      }`);
+
+    const { edges } = data.allMarkdownRemark;
+
+      console.log(data);
+
     return (
         <Layout>
             <h1>My Blog</h1>
             <p>This is the blog page</p>
+            <hr/>
+            <ol>
+                {
+                    edges.map(item => (
+                        <li><p>{item.node.frontmatter.title} <br/> <small>{item.node.frontmatter.date}</small></p></li>
+                    ))
+                }
+            </ol>
         </Layout>
     )
 }
